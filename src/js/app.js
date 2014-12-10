fa0850b925bca85f950f3724736006af0052d3f5
Josef Vybíral <josef.vybiral@gmail.com>
2014-12-10 17:47:05 +0100

    Nástřel redesignu úvodky

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,15 @@
 // Place any jQuery/custom code in here.
 $(function() {
+  var okno = $( window ).height(),
+      minHeight = 480;
+  $( '.uvodka nav' ).height(okno);
 
+  $(window).on('resize', function(){
+      okno = $( window ).height();
+
+      if (okno >= minHeight) {
+        $( '.uvodka nav' ).height(okno);
+      }
+  });
 });
 
