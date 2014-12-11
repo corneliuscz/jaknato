4848ffa84ea82f258353522cb33f107e52098d3a
Josef Vybíral <josef.vybiral@gmail.com>
2014-12-11 15:16:03 +0100

    Hlavička v 0.5

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,20 @@
 // Place any jQuery/custom code in here.
 $(function() {
+  /*
+   * Yjist9me si velikost okna a podle toho natáhneme sloupec s fotkou v navigaci
+   */
   var okno = $( window ).height(),
+      hlavicka = $( '.header' ).height(),
+      padding =  64; // Padding v hlavičce
       minHeight = 480;
-  $( '.uvodka nav' ).height(okno);
+  $( '.uvodka nav' ).height(okno - hlavicka - padding);
 
   $(window).on('resize', function(){
       okno = $( window ).height();
+      hlavicka = $( '.header' ).height();
 
       if (okno >= minHeight) {
-        $( '.uvodka nav' ).height(okno);
+        $( '.uvodka nav' ).height(okno - hlavicka - padding);
       }
   });
 });
