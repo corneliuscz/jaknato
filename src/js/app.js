11012d0e206841d97556a0b84cf96816685bf639
Josef Vybíral <josef.vybiral@gmail.com>
2015-02-06 09:36:17 +0100

    JSHint settings

--- src/js/app.js
+++ src/js/app.js
@@ -1,78 +1,7 @@
 /*global $, Modernizr, console */
 
-
-/*****************************************************************
- * Flexbox fallback START
- */
-/*
-// http://stackoverflow.com/questions/11306736/jquery-check-if-element-has-a-specific-style-property-defined-inline
-$.fn.inlineStyle = function (prop) {
-  var styles = this.attr("style"),
-      value;
-  styles && styles.split(";").forEach(function (e) {
-    var style = e.split(":");
-    if ($.trim(style[0]) === prop) {
-      value = style[1];
-    }
-  });
-  return value;
-};
-
-function setWidth(){
-  $('.no-flexbox .container--uvodka nav').not('.vertical').each(function(){
-    var $el = $(this),
-        widthPerBox = Math.ceil(100/$el.find('> nav div').length);
-
-    $el.children('nav div').each(function(){
-      if($(this).attr('data-width') !== undefined ){
-        $(this).css('width', $(this).data('width')+'%');
-      }
-      // do not overwrite the actual width
-      else if(!$(this).inlineStyle('width')){
-        $(this).css('width', widthPerBox+'%');
-      }
-    });
-  });
-}
-
-// there is a problem while setting width and height at same time
-// so I'm setting width then height
-function setHeight(){
-  $('.no-flexbox .container--uvodka nav').not('.vertical').each(function(){
-    var $el = $(this),
-        maxHeight = 0;
-
-    $el.children('nav div').each(function(){
-      // set height auto to reset the height
-      $(this).css('height', 'auto');
-      if($(this).height() > maxHeight){
-        maxHeight = $(this).outerHeight();
-      }
-    });
-    // makes sure maxHeight is not equal to 0
-    if(maxHeight){
-      $el.find(' >nav div').css('height', maxHeight);
-    }
-  });
-}
-
-$(document).ready(function(){
-  setWidth();
-  setHeight();
-});
-$(window).resize(function(){
-  setHeight();
-});
-*/
-/*
- * Flexbox fallback END
- *******************************************************************/
-
-
 function resizeNav(el, head, headPadding, minHeight) {
-
   'use strict';
-
   var okno = $(window).height(),
       hlavicka = $(head).height();
 
@@ -93,7 +22,7 @@ function checkMq() {
   if (Modernizr.mq('only screen and (min-width: 1140px)')) {
     resizeNav(elem, headerElem, padding, minHeight);
   } else {
-    if (!$("html").hasClass("no-flexbox")) {
+    if (!$('html').hasClass('no-flexbox')) {
       $(elem).removeAttr( 'style' );
     }
   }
@@ -112,6 +41,8 @@ $(function () {
   $(window).resize(function () {
     checkMq();
   });
+
+  console.log ('kokote');
 
   /*
   if (breakpoint === 1) {
